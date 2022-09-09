8af7611c865f08d16cd24d4661ea07159df822d5
pryority <matthewapryor@gmail.com>
2022-09-09 05:43:59 -0400

    fix: some styling screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { createClient } from 'urql';
 import delegateList from './data/delegates/delegates.json';
 import names from './data/delegates/names.json';
 // (i.e. ``http:/\/localhost:8545``)
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${import.meta.env.REACT_APP_INFURA_API_KEY}`);
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY}`);
 
 // The provider also allows signing transactions to
 // send ether and pay to change state within the blockchain.
@@ -59,8 +59,9 @@ function App() {
   const [logs, setLogs] = useState([]);
   const [transferLogs, setTransferLogs] = useState([]);
   const [vp, setVP] = useState(0);
-  const [delegate, setDelegate] = useState("")
+  const [delegate, setDelegate] = useState("0x81b287c0992B110ADEB5903Bf7E2d9350C80581a")
   const [delegateList, setDelegateList] = useState([]);
+  const [isShowingAll, setIsShowingAll] = useState(false);
 
   const getBalance = async () => {
     getETHBalance();
@@ -151,6 +152,10 @@ function App() {
     setVP(logsFrom);
   }
 
+
+  const toggle = () => {
+    setIsShowingAll(!isShowingAll);
+  }
   // console.log(delegateList)
 
   useEffect(() => {
@@ -165,29 +170,53 @@ function App() {
     <div className="flex flex-col h-full justify-start items-center bg-gradient-to-tr from-blue-500/20 to-sky-400/20 relative">
       <div className='z-50 block w-full justify-center items-center bg-sky-500 text-white p-4 '>
         <div className="flex justify-between items-center">
-          <h1 className="w-full font-black uppercase tracking-tighter">ENS Delegates</h1>
+          <h1 className="w-full font-black uppercase text-4xl md:text-6xl tracking-tighter">ENS Delegates</h1>
           <p className="w-2/3 text-end leading-4">A visualization tool for ENS governance allocation.</p>
         </div>
       </div>
+      <div className={isShowingAll ? ('z-50 block w-full justify-center items-center bg-stone-50 hover:opacity-80 text-black/50 cursor-pointer p-[0.62] px-4') : ('z-50 block w-full justify-center items-center bg-gradient-to-b from-stone-50 to-stone-50/20 hover:opacity-80 text-black/50 cursor-pointer p-[0.62] px-4')}
+        onClick={toggle}
+      >
+        <div className="flex justify-between items-center">
+          <p className="w-full font-black uppercase tracking-tighter">{isShowingAll ? 'Hide All' : 'View All'}</p>
+        </div>
+      </div>
+      {isShowingAll && (
+        <div className='z-50 block w-full justify-center items-center bg-stone-50 text-black/50 p-4'
+        >
+          <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-10 justify-center items-center overflow-y-scroll">
+            {names.map((d, i) =>
+              <div className=" p-1">
+                <a href={`https://alpha.ens.domains/profile/${d}`} target='_blank'>
+                  <div className="w-20 h-20 md:w-32 md:h-32 rounded-md text-white bg-gradient-to-bl from-blue-500 to-sky-600 border relative cursor-pointer">
+                    <p className="font-bold w-5/6 text-end tracking-tighter absolute bottom-2 right-2 truncate"><span className="font-black">{d.replace('.eth', '')}</span><span className="opacity-75">.eth</span></p>
+                    <img decoding="async" src={`https://metadata.ens.domains/mainnet/avatar/${d}`} className="rounded"></img>
+                  </div>
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
       <div className="flex w-full h-screen mt-8">
         <div className="flex flex-col w-full space-y-4 items-center p-2 bg-sky-50/20;">
-          <div className="flex items-center space-x-8 w-full px-8">
-            <div className="tracking-wider font-medium text-2xl flex flex-col w-2/5">
-              <div className="flex items-center">
-                <p className="truncate w-2/3 font-bold text-end text-3xl">{delegate}{import.meta.env.VITE_MSG}</p>
+          <div className="flex flex-col md:flex-row items-center space-x-8 w-full px-8">
+            <div className="tracking-wider font-medium text-2xl flex space-x-4 md:spacing-x-0 md:flex-col md:w-3/5 w-2/5">
+              <div className="flex items-center w-full">
+                <p className="truncate w-1/6 md:w-2/3 font-bold text-end text-3xl">{delegate}{ }</p>
                 <p>'s</p>
               </div>
-              <p className="tracking-tight">Delegate Vote Changes</p>
-              <p className="tracking-tighter font-semibold mt-4">Voting Power: <span>{vp}</span></p>
+              <p className="tracking-tight text-base">Delegate Vote Changes</p>
+              <p className="tracking-tighter font-semibold text-base mt-4">Voting Power: <span>{vp}</span></p>
             </div>
-            <form className="flex w-full bg-stone-50 rounded-md p-2 border"
+            <form className="flex w-full bg-stone-50 rounded-md border"
               onSubmit={handleSubmit}>
-              <input placeholder="Enter a Delegate.eth" className=" bg-transparent p-1 px-2 flex w-full text-black rounded-l-md focus:ring-none focus:outline-none"
+              <input placeholder="Enter a Delegate.eth" className="bg-transparent p-1 px-2 flex w-full text-black rounded-l-md focus:ring-none focus:outline-none"
                 onChange={handleChange} />
               <button type="submit" className="bg-blue-800 flex px-2 items-center text-white font-semibold rounded-md">Search</button>
             </form>
           </div>
-          <Marquee gradientColor={[8, 30, 48]} className='rounded-full border-2 border-sky-800/40 items-center overflow-clip' speed={24}>
+          <Marquee gradientWidth={'62px'} gradientColor={[8, 30, 48]} className='rounded-full border-2 border-sky-800/40 items-center overflow-clip' speed={24}>
             {names.map((d, i) =>
             (<div key={i} className="flex items-center space-x-1 mr-4 p-2"
               onClick={async () => {
@@ -225,7 +254,7 @@ function App() {
             }
             >0x3b3525f60eeea4a1ef554df5425912c2a532875d</p>
           </div> */}
-          <div className="grid grid-cols-4 xl:grid-cols-6 p-4 h-full overflow-y-scroll bg-inherit">
+          <div className="grid grid-cols-3 md:grid-cols-4 xl:grid-cols-6 md:p-4 h-full overflow-y-scroll bg-inherit">
             {/* {transferLogs.map((log, i) => (
               <div className="flex flex-col p-1 items-center">
                 <div className="bg-gradient-to-b from-purple-500 via-purple-400 to-purple-500 shadow-lg p-2 rounded-md flex-col w-full cursor-pointer h-full items-center hover:scale-[.98] hover:shadow-lg hover:opacity-80 border-2 hover:animate-pulse">
@@ -257,7 +286,7 @@ function App() {
             {logs.map((log, i) => (
               <div className="flex flex-col p-1">
                 {(log.args.newBalance > log.args.previousBalance) ? (
-                  <div className="bg-green-500 shadow-lg p-2 rounded-md flex-col w-full cursor-pointer h-full items-center hover:scale-[.98] hover:shadow-lg hover:opacity-80 border-2 hover:animate-pulse">
+                  <div className="bg-green-500 shadow-lg p-2 rounded-md flex-col md:w-full cursor-pointer h-full items-center hover:scale-[.98] hover:shadow-lg hover:opacity-80 space-y-4 border-2 hover:animate-pulse">
                     <div className=" flex w-full space-x-2 items-center ">
                       <div className="justify-center items-center flex">
                         <a href={`https://etherscan.io/tx/${log.transactionHash}`} className="bg-indigo-400 text-white h-6 w-6 rounded-full justify-center items-center" target="_blank">
@@ -266,35 +295,37 @@ function App() {
                       </div >
                       <div className="truncate">{log.transactionHash}</div>
                     </div >
-                    <div className="flex w-full mt-4 justify-between">
+                    <div className="flex flex-col md:flex-row w-full mt-4 justify-between">
                       <p>Balance:</p>
-                      <p className="truncate w-1/3">{ethers.utils.formatUnits(log.args.newBalance._hex)}</p>
+                      <p className="truncate w-5/6 md:w-2/3">{ethers.utils.formatUnits(log.args.newBalance._hex)}</p>
                     </div>
-                    <div className="flex w-full  justify-center items-center">
-                      <p className="truncate w-1/3 font-bold">{(log.args.newBalance > log.args.previousBalance) ? ('+') : ('-')}{(ethers.utils.formatUnits(log.args.newBalance._hex) - ethers.utils.formatUnits(log.args.previousBalance._hex))}</p>
+                    <div className="flex flex-col md:flex-row w-full  justify-center items-center">
+                      <p className="truncate text-center w-5/6 md:w-1/3 font-bold">{(log.args.newBalance > log.args.previousBalance) ? ('+') : ('-')}{(ethers.utils.formatUnits(log.args.newBalance._hex) - ethers.utils.formatUnits(log.args.previousBalance._hex))}</p>
                       <p className="font-light uppercase text-sm">Votes</p>
                     </div>
                     <p className="bg-stone-50 text-center text-stone-500 border text-xs rounded-md mx-4">Receive</p>
                   </div >
-                ) : (<div className="bg-red-500 shadow-lg  text-white p-2 rounded-md flex-col w-full cursor-pointer h-full items-center hover:scale-[.98] hover:shadow-lg hover:opacity-80 border-2 hover:animate-pulse">
+                ) : (<a href={`https://etherscan.io/tx/${log.transactionHash}`} target='_blank'
+                  className="bg-red-500 shadow-lg  text-white p-2 rounded-md flex-col w-full cursor-pointer space-y-4 h-full justify-center items-center hover:text-white hover:scale-[.98] hover:shadow-lg hover:opacity-80 border-2 hover:animate-pulse">
                   <div className=" flex w-full space-x-2 items-center ">
                     <div className="justify-center items-center flex">
-                      <a href={`https://etherscan.io/tx/${log.transactionHash}`} className="bg-indigo-400 text-white h-6 w-6 rounded-full justify-center items-center">
+                      <div
+                        className="bg-indigo-400 text-white h-6 w-6 rounded-full justify-center items-center">
                         <p className="text-center text-xs font-black mt-[4px]">{[i + 1]}</p>
-                      </a>
+                      </div>
                     </div>
                     <div className="truncate">{log.transactionHash}</div>
                   </div>
-                  <div className="flex w-full mt-4 justify-between">
-                    <p>Balance:</p>
-                    <p className="truncate w-1/3">{ethers.utils.formatUnits(log.args.newBalance._hex)}</p>
+                  <div className="flex flex-col md:flex-row w-full mt-4 justify-between">
+                    <p className="">Balance:</p>
+                    <p className="truncate w-5/6 md:w-2/3">{ethers.utils.formatUnits(log.args.newBalance._hex)}</p>
                   </div>
-                  <div className="flex w-full justify-center items-center">
-                    <p className="truncate w-1/3 font-bold">{(log.args.newBalance > log.args.previousBalance) && ('+')}{((ethers.utils.formatUnits(log.args.newBalance._hex) - ethers.utils.formatUnits(log.args.previousBalance._hex)))}</p>
+                  <div className="flex flex-col md:flex-row w-full justify-center items-center">
+                    <p className="truncate text-center w-5/6 md:w-1/3 font-bold">{(log.args.newBalance > log.args.previousBalance) && ('+')}{((ethers.utils.formatUnits(log.args.newBalance._hex) - ethers.utils.formatUnits(log.args.previousBalance._hex)))}</p>
                     <p className="font-light uppercase text-sm">Votes</p>
                   </div>
                   <p className="bg-stone-50 text-center text-stone-500 border text-xs rounded-md mx-4">Spend</p>
-                </div>)
+                </a>)
                 }
               </div >
             ))}
